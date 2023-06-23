6b14699881999c3bcb84759725cc1d1e247aff17
Loureiro <72601341+TLoureiro07@users.noreply.github.com>
2023-06-23 15:33:14 +0100

    Add files via upload

---
+++ stoned-scoreboard/client.lua
@@ -0,0 +1,122 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+-- Codigo
+
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded')
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    isLoggedIn = true
+
+    ESX.TriggerServerCallback('qb-scoreboard:server:GetConfig', function(config)
+        Config.IllegalActions = config
+    end)
+end)
+
+local scoreboardOpen = false
+
+GetPlayers = function()
+    local players = {}
+    for _, player in ipairs(GetActivePlayers()) do
+        local ped = GetPlayerPed(player)
+        if DoesEntityExist(ped) then
+            table.insert(players, player)
+        end
+    end
+    return players
+end
+
+
+Citizen.CreateThread(function()
+    --TriggerEvent('chat:addSuggestion', '/ilegal', 'Autorizar ou proibir a atividade ilegal no servidor', {
+        --{ name="ON/OFF", help="ON - Permitir RP Agressivo | OFF - Proíbir RP Agressivo" },
+    --})
+    
+    while true do
+        local isActivatorPressed = IsControlJustPressed(0, 57)
+        local isSecondaryPressed =  IsControlPressed(0, 57)
+        local isPauseESCPressed = IsControlJustPressed(0, 322)
+        local isPausePPressed = IsControlJustPressed(0, 199)
+        if isActivatorPressed and isSecondaryPressed then
+            if not scoreboardOpen then
+                ESX.TriggerServerCallback('qb-scoreboard:server:GetActiveCops', function(cops)
+                    Config.CurrentCops = cops
+                    ESX.TriggerServerCallback('qb-scoreboard:server:Estado', function(estado)
+                        ESX.TriggerServerCallback('qb-scoreboard:server:GetActiveINEM', function(INEM)
+                            Config.currentINEM = INEM
+                            if estado == "on" then
+                                local paramedicsOnline = 0
+                            
+                                for _, player in ipairs(GetActivePlayers()) do
+                                    local xPlayer = ESX.GetPlayerData(player)
+                            
+                                    if xPlayer.job.name == Config.ambulancejob then
+                                        paramedicsOnline = paramedicsOnline + 1
+                                    end
+                                end
+                            
+                                print("Paramedics Online: " .. paramedicsOnline) -- Verifique se a contagem está correta no console
+                            
+                                SendNUIMessage({
+                                    action = "open",
+                                    estado = "on",
+                                    players = GetCurrentPlayers(),
+                                    maxPlayers = Config.MaxPlayers,
+                                    requiredCops = Config.IllegalActions,
+                                    requiredParamedics = Config.ambulancejob,
+                                    currentCops = Config.CurrentCops,
+                                    currentParamedics = paramedicsOnline,
+                                })
+                            elseif estado == "off" then
+                                SendNUIMessage({
+                                    action = "open",
+                                    estado = "off",
+                                    players = GetCurrentPlayers(),
+                                    maxPlayers = Config.MaxPlayers,
+                                    requiredCops = Config.IllegalActions,
+                                    requiredParamedics = Config.ambulancejob,
+                                    currentCops = Config.CurrentCops,
+                                    currentParamedics = 0,
+                                })
+                            end
+                            
+                            
+                            scoreboardOpen = true
+                        end)
+                    end)
+                end)
+            end
+        end
+
+        if (isActivatorPressed and isSecondaryPressed) or isPauseESCPressed or isPausePPressed then
+            if scoreboardOpen then
+                SendNUIMessage({
+                    action = "close",
+                })
+                scoreboardOpen = false
+            end
+        end
+
+        Citizen.Wait(5)
+    end
+end)
+
+function GetCurrentPlayers()
+    local TotalPlayers = 0
+
+    for _, player in ipairs(GetActivePlayers()) do
+        TotalPlayers = TotalPlayers + 1
+    end
+
+    return TotalPlayers
+end
+
+RegisterNetEvent('qb-scoreboard:client:SetActivityBusy')
+AddEventHandler('qb-scoreboard:client:SetActivityBusy', function(activity, busy)
+    Config.IllegalActions[activity].busy = busy
+end)
---
+++ stoned-scoreboard/config.lua
@@ -0,0 +1,69 @@
+Keys = {
+    ['ESC'] = 322, ['F1'] = 288, ['F2'] = 289, ['F3'] = 170, ['F5'] = 166, ['F6'] = 167, ['F7'] = 168, ['F8'] = 169, ['F9'] = 56, ['F10'] = 57,
+    ['~'] = 243, ['1'] = 157, ['2'] = 158, ['3'] = 160, ['4'] = 164, ['5'] = 165, ['6'] = 159, ['7'] = 161, ['8'] = 162, ['9'] = 163, ['-'] = 84, ['='] = 83, ['BACKSPACE'] = 177,
+    ['TAB'] = 37, ['Q'] = 44, ['W'] = 32, ['E'] = 38, ['R'] = 45, ['T'] = 245, ['Y'] = 246, ['U'] = 303, ['P'] = 199, ['['] = 39, [']'] = 40, ['ENTER'] = 18,
+    ['CAPS'] = 137, ['A'] = 34, ['S'] = 8, ['D'] = 9, ['F'] = 23, ['G'] = 47, ['H'] = 74, ['K'] = 311, ['L'] = 182,
+    ['LEFTSHIFT'] = 21, ['Z'] = 20, ['X'] = 73, ['C'] = 26, ['V'] = 0, ['B'] = 29, ['N'] = 249, ['M'] = 244, [','] = 82, ['.'] = 81,
+    ['LEFTCTRL'] = 36, ['LEFTALT'] = 19, ['SPACE'] = 22, ['RIGHTCTRL'] = 70,
+    ['HOME'] = 213, ['PAGEUP'] = 10, ['PAGEDOWN'] = 11, ['DELETE'] = 178,
+    ['LEFT'] = 174, ['RIGHT'] = 175, ['TOP'] = 27, ['DOWN'] = 173,
+}
+
+Config = Config or {}
+
+-- Open scoreboard key
+Config.OpenKey = Keys["F10"]
+
+-- Link da imagem do cabeçalho
+Config.ScoreboardHeaderImage = "https://images7.alphacoders.com/587/587535.jpg"
+
+
+-- Max Server Players
+Config.MaxPlayers = GetConvarInt('sv_maxclients', 200) -- It returnes 64 if it cant find the Convar Int
+
+-- Jobs names
+
+Config.policejob1 = 'police'
+Config.policejob2 = 'sheriff'
+Config.ambulancejob = 'ambulance'
+
+-- Minimum Police for Actions
+Config.IllegalActions = {
+    ["storerobbery"] = {
+        minimum = 1,
+        busy = false,
+    },
+    ["bankrobbery"] = {
+        minimum = 5,
+        busy = false,
+    },
+    ["jewellery"] = {
+        minimum = 5,
+        busy = false,
+    },
+    ["pacific"] = {
+        minimum = 6,
+        busy = false,
+    },
+}
+
+-- Minimum Paramedics 
+Config.INEM = {
+    {
+        job = Config.ambulancejob,
+        minimum = 1 -- minimum number of online paramedics to show the check icon
+    },
+    
+}
+
+-- Current Cops Online
+Config.CurrentCops = 0
+
+Config.CurrentINEM = 0
+
+Controls = {
+    Activator = { -- Open/Close Radio
+        Name = "INPUT_MP_TEXT_CHAT_TEAM", -- Control name
+        Key = 213, -- home
+    },
+}
